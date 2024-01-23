95b8c7ae60c69b2a5f4d1e6574791aa7cc5bc898
JaMesLiMers <jameslimers@gmail.com>
2024-01-23 23:01:38 +0800

    Yiming: fix bug

--- Godot/Scenes/SeperateFuncByYiming/Src/character_manager.gd
+++ Godot/Scenes/SeperateFuncByYiming/Src/character_manager.gd
@@ -28,7 +28,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    delta = delta # we may use it later 
     
     # trigger by input
     if Input.is_action_just_pressed("player_power_separate"):
