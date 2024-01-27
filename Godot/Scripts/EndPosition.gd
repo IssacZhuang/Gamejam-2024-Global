033ab25a43f6b2f202a26b8db6e3f47dd6a7e8fe
Yilin Liu <yilin.liu1999@gmail.com>
2024-01-28 03:47:27 +0800

    feat: 正确显示旗帜并且可以碰撞

--- Godot/Scripts/EndPosition.gd
+++ Godot/Scripts/EndPosition.gd
@@ -9,7 +9,7 @@ enum endpoint_type {
 
 @export var endpointType = endpoint_type.BothEnd
 @export var is_active = false
-
+@export var texture: Texture2D
 var effectManager: Node2D
 var soundManager: Node2D
 var is_done = false
@@ -19,9 +19,13 @@ func _ready():
 	effectManager = get_node("/root/Node2D/EffectManagerNode2D")
 	soundManager = get_node("/root/Node2D/SoundManagerNode2D")
 	is_done = false
-	
+	var children = get_children()
+	for c in children:
+		if c.name == "Sprite2D":
+			c.texture = texture
 
 func _on_character_body_entered_end_point(body):
+	print("end")
 	if body.name == "CharacterBoth":
 		done_level(body)
 	elif body.name == "Character1":
@@ -35,3 +39,8 @@ func done_level(body):
 		soundManager.stop_music()
 		soundManager.play_music("bgm_end")
 		is_done = true
+
+
+
+
+
