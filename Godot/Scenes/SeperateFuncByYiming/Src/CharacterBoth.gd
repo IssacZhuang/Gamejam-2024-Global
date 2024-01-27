0f72bf3852a01b054446e89206e9aef9791db653
Yilin Liu <yilin.liu1999@gmail.com>
2024-01-27 19:22:05 +0800

    feat: 添加冲刺无敌时间

--- Godot/Scenes/SeperateFuncByYiming/Src/CharacterBoth.gd
+++ Godot/Scenes/SeperateFuncByYiming/Src/CharacterBoth.gd
@@ -6,11 +6,11 @@ var is_active = true
 @export var thrust: Vector2 = Vector2(0, -400)
 @export var torque = 700
 @export var initial_force: Vector2 = Vector2(3000, -4000)
-
-@export var cooldown_time = 5 #冷却时间,单位秒
+@export var cooldown_time = 2 #冷却时间,单位秒
+@export var invincible_time = 0.5 #冲刺后无敌时间
 var cooldown_left = 0
 var can_burst: bool
-
+var is_invincible: bool
 signal burst
 
 # Called when the node enters the scene tree for the first time.
@@ -24,6 +24,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	print(is_invincible)
 	if cooldown_left > 0:
 		cooldown_left -= delta
 	
@@ -46,22 +47,26 @@ func _integrate_forces(state):
 			None
 	"""
 	if can_burst and Input.is_action_just_pressed("player_power_down"):
-		print("player_power_down")
+		#print("player_power_down")
 		#state.apply_force(thrust.rotated(rotation))
 		state.apply_impulse(thrust.rotated(rotation+1.57))
 		cooldown_left = cooldown_time
 		burst.emit()
+		start_invincible()
 	else:
 		state.apply_force(Vector2())
 	var rotation_direction = 0
 	if Input.is_action_pressed("player_rotation_left"):
-		print("player_rotation_left")
+		#print("player_rotation_left")
 		rotation_direction -= 1
 	if Input.is_action_pressed("player_rotation_right"):
-		print("player_rotation_right")
+		#print("player_rotation_right")
 		rotation_direction += 1
 	state.apply_torque(rotation_direction * torque)
-	
+func start_invincible():
+	is_invincible = true
+	await get_tree().create_timer(1.0).timeout
+	is_invincible = false
 func on_separate():
 	is_active = false
 
